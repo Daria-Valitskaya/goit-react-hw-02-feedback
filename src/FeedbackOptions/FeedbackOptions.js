7c0b14a4286217eb07bf88ef963f6380103e9fd7
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-08-27 23:56:31 +0300

    done

--- src/FeedbackOptions/FeedbackOptions.js
+++ src/FeedbackOptions/FeedbackOptions.js
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
+import style from "./FeedbackOptions.module.css";
 export default function FeedbackOption({ options, handleFeedback }) {
   return (
-    <div>
+    <div className={style.btn__wrapper}>
       {options.map((option, index) => (
         <button
           type="button"
